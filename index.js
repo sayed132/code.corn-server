98de6393bbc4ad30bdd650f9db387ddf0e13a6d2
Abu Sayed <rafsansayed132@gmail.com>
2022-10-24 23:04:55 +0600

    set server

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
-// const express = require('express')
-// const app = express();
-// const cors = require('cors');
-// const port = process.env.PORT || 5000;
+const express = require('express')
+const app = express();
+const cors = require('cors');
+const port = process.env.PORT || 5000;
 
 // app.use(cors());
 // const categories = require('./Data/Categories.json');
